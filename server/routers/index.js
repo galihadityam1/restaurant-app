d289545ee01b46fefddb157292b3a9bf8e20ba2c
galihadityam1 <galihadityam1@gmail.com>
2024-03-14 21:38:36 +0700

    adding vite with tailwind installation

---
+++ server/routers/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const UserController = require("../controllers/UserController");
+const loginValidation = require("../middlewares/authentication");
+const Controller = require("../controllers/Controller");
+const router = express.Router();
+
+router.post("/login", UserController.login);
+router.post("/register", UserController.register);
+
+router.use(loginValidation)
+router.get("/menu", Controller.getAllMenu);
+router.post("/menu", Controller.postMenu);
+router.put("/menu/:id", Controller.editMenu);
+router.delete("/menu/:id", Controller.deleteMenu);
+router.post("/menu/:id", Controller.addOrder);
+
+module.exports = router;
